af0585a535a941d243eadd2531279bf16968167d
Muzakka123 <159703338+Muzakka123@users.noreply.github.com>
2024-05-12 16:24:41 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var guestArray = ["hina", "mehak", "wajeeha"];
+guestArray.map(function (items) { return console.log("Hello,".concat(items, " you are invited to dinner")); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let guestArray:string[] = ["hina","mehak","wajeeha"];
+
+guestArray.map((items)=> console.log(`Hello,${items} you are invited to dinner`));
